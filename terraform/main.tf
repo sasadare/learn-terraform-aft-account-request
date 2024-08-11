9e53eb17d3d9ea4bd40cb26ebb6a74e9400739b4
Shubhangi <sasadare@amazon.com>
2024-08-11 21:29:17 +0000

    upgate

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sasadare+TestAccounta@amazon.com"
-    AccountName               = "TestAccounta"
+    AccountEmail              = "sasadare+TestAccountab@amazon.com"
+    AccountName               = "TestAccountab"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "sasadare@amazon.com"
     SSOUserFirstName          = "Shubhangi"
