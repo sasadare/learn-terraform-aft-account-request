664dff84aa6742cded155bd8e237e238b746d584
Shubhangi <sasadare@amazon.com>
2024-08-11 21:58:17 +0000

    'update'

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-# module "sandbox" {
-#   source = "./modules/aft-account-request"
+module "sandbox" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail              = "sasadare+TestAccountab@amazon.com"
-#     AccountName               = "TestAccountab"
-#     ManagedOrganizationalUnit = "Sandbox"
-#     SSOUserEmail              = "sasadare@amazon.com"
-#     SSOUserFirstName          = "Shubhangi"
-#     SSOUserLastName           = "Sadare"
-#   }
+  control_tower_parameters = {
+    AccountEmail              = "sasadare+TestAccount@amazon.com"
+    AccountName               = "Test-Account"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "sasadare@amazon.com"
+    SSOUserFirstName          = "Shubhangi"
+    SSOUserLastName           = "Sadare"
+  }
 
-#   account_tags = {
-#     "Learn Tutorial tag01" = "AFT"
-#   }
+  account_tags = {
+    "Learn Tutorial tag01" = "AFT"
+  }
 
-#   change_management_parameters = {
-#     change_requested_by = "HashiCorp Learn"
-#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-#   }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
 
-#   custom_fields = {
-#     group = "non-prod"
-#   }
+  custom_fields = {
+    group = "non-prod"
+  }
 
-#   account_customizations_name = "sandbox"
-# }
+  account_customizations_name = "sandbox"
+}
