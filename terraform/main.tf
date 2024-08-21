2a9e721214d372c1a7126bd24758bea78075eb4d
Shubhangi <sasadare@amazon.com>
2024-08-21 06:37:25 +0000

    email update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sasadare+Test01@amazon.com"
+    AccountEmail              = "sasadare+Testab@amazon.com"
     AccountName               = "Test"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "sasadare+Test@amazon.com"
