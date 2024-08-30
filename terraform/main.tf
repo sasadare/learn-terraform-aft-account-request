01812915bb972c842c58bbb9fdcd00fb45e98b37
Shubhangi <sasadare@amazon.com>
2024-08-30 13:00:54 +0000

    creation of new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,31 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
+},
+{
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sasadare+shubh@amazon.com"
+    AccountName               = "Shubhangi"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "sasadare+shubh@amazon.com"
+    SSOUserFirstName          = "Shubh"
+    SSOUserLastName           = "Shubhangi"
+  }
+
+  account_tags = {
+    "Learn Tutorial tag01" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
 }
