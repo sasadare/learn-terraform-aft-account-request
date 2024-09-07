334cb42646032b9d194c13089beeac33f62be4fc
Shubhangi <sasadare@amazon.com>
2024-09-07 17:27:07 +0000

    ghfdg

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,9 @@ module "sandbox" {
     AccountEmail              = "sasadare+tryoutfinal@amazon.com"
     AccountName               = "tryoutfinal"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "sasadare@amazon.com"
-    SSOUserFirstName          = "Shubh"
-    SSOUserLastName           = "analysis"
+    SSOUserEmail              = "sasadare+tryoutfinal@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
   }
 
   account_tags = {
@@ -25,30 +25,30 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
-module "sandbox01" {
-  source = "./modules/aft-account-request"
+# module "sandbox01" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "sasadare+shubh@amazon.com"
-    AccountName               = "Shubhangi"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "sasadare+shubh@amazon.com"
-    SSOUserFirstName          = "Shubh"
-    SSOUserLastName           = "Shubhangi"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "sasadare+shubh@amazon.com"
+#     AccountName               = "Shubhangi"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "sasadare+shubh@amazon.com"
+#     SSOUserFirstName          = "Shubh"
+#     SSOUserLastName           = "Shubhangi"
+#   }
 
-  account_tags = {
-    "Learn Tutorial tag01" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial tag01" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
