1dd11aa1f09f4703cef75c95ea44add9aa15fc5d
Shubhangi <sasadare@amazon.com>
2024-09-12 16:30:40 +0000

    removing an account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,17 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "sasadare+tryoutfinal@amazon.com"
-    AccountName               = "tryoutfinal"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "sasadare+tryoutfinal@amazon.com"
-    SSOUserFirstName          = "Admin"
-    SSOUserLastName           = "User"
-  }
-
-  account_tags = {
-    "Learn Tutorial tag01" = "AFT02"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-# module "sandbox01" {
+# module "sandbox" {
 #   source = "./modules/aft-account-request"
 
 #   control_tower_parameters = {
-#     AccountEmail              = "sasadare+shubh@amazon.com"
-#     AccountName               = "Shubhangi"
+#     AccountEmail              = "sasadare+tryoutfinal@amazon.com"
+#     AccountName               = "tryoutfinal"
 #     ManagedOrganizationalUnit = "Sandbox"
-#     SSOUserEmail              = "sasadare+shubh@amazon.com"
-#     SSOUserFirstName          = "Shubh"
-#     SSOUserLastName           = "Shubhangi"
+#     SSOUserEmail              = "sasadare+tryoutfinal@amazon.com"
+#     SSOUserFirstName          = "Admin"
+#     SSOUserLastName           = "User"
 #   }
 
 #   account_tags = {
-#     "Learn Tutorial tag01" = "AFT"
+#     "Learn Tutorial tag01" = "AFT02"
 #   }
 
 #   change_management_parameters = {
@@ -52,3 +25,30 @@ module "sandbox" {
 
 #   account_customizations_name = "sandbox"
 # }
+# # module "sandbox01" {
+# #   source = "./modules/aft-account-request"
+
+# #   control_tower_parameters = {
+# #     AccountEmail              = "sasadare+shubh@amazon.com"
+# #     AccountName               = "Shubhangi"
+# #     ManagedOrganizationalUnit = "Sandbox"
+# #     SSOUserEmail              = "sasadare+shubh@amazon.com"
+# #     SSOUserFirstName          = "Shubh"
+# #     SSOUserLastName           = "Shubhangi"
+# #   }
+
+# #   account_tags = {
+# #     "Learn Tutorial tag01" = "AFT"
+# #   }
+
+# #   change_management_parameters = {
+# #     change_requested_by = "HashiCorp Learn"
+# #     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+# #   }
+
+# #   custom_fields = {
+# #     group = "non-prod"
+# #   }
+
+# #   account_customizations_name = "sandbox"
+# # }
