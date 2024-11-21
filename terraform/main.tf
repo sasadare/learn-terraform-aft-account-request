1d71b5ec8a03c376b02565018eef509be0057fe6
Shubhangi <sasadare@amazon.com>
2024-11-21 05:32:08 +0000

    Account creation

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+module "aft_vending_10" {
+   source = "./modules/aft-account-request"
+ 
+   control_tower_parameters = {
+     AccountEmail              = "aftbootcamptestt+sasadare@amazon.com"
+     AccountName               = "AFT-Vending10"
+     ManagedOrganizationalUnit = "AFT" 
+     SSOUserEmail              = "aftbootcamptestt+sasadare@amazon.com"
+     SSOUserFirstName          = "AFT"
+     SSOUserLastName           = "Vending10"
+   }
+ 
+   account_tags = {
+     "ABC:Owner"       = "myname@mycompany.com"
+     "ABC:Division"    = "ENT"
+     "ABC:Environment" = "Dev"
+     "ABC:CostCenter"  = "123456"
+     "ABC:Vended"      = "true"
+     "ABC:DivCode"     = "102"
+     "ABC:BUCode"      = "ABC003"
+     "ABC:Project"     = "123456"
+   }
+ 
+   change_management_parameters = {
+     change_requested_by = "AWS Control Tower Lab"
+     change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+   }
+ 
+   custom_fields = {
+     custom1 = "Dedsec"
+   }
+ 
+   account_customizations_name = "sandbox"
+ }
