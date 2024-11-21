9d8d6bfd5b3a01125bc7da66a2c698c8aabad34e
Shubhangi <sasadare@amazon.com>
2024-11-21 08:42:48 +0000

    existing account enrol

---
+++ terraform/existing.tf
@@ -0,0 +1,34 @@
+module "aft_vending_10" {
+   source = "./modules/aft-account-request"
+ 
+   control_tower_parameters = {
+     AccountEmail              = "bootcamp+sasadare@amazon.com"
+     AccountName               = "Bootcamp"
+     ManagedOrganizationalUnit = "Sandbox(ou-401u-g3zh8v7z)" 
+     SSOUserEmail              = "bootcamp+sasadare@amazon.com"
+     SSOUserFirstName          = "Admin"
+     SSOUserLastName           = "User"
+   }
+ 
+   account_tags = {
+     "ABC:Owner"       = "myname@mycompany.com"
+     "ABC:Division"    = "ENT"
+     "ABC:Environment" = "Dev"
+     "ABC:CostCenter"  = "123456"
+     "ABC:Vended"      = "true"
+     "ABC:DivCode"     = "102"
+     "ABC:BUCode"      = "ABC003"
+     "ABC:Project"     = "123456"
+   }
+ 
+   change_management_parameters = {
+     change_requested_by = "AWS Control Tower Lab"
+     change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+   }
+ 
+   custom_fields = {
+     custom1 = "Dedsec"
+   }
+ 
+   account_customizations_name = "sandbox"
+ }
